6a2552a012cd32c7b2f046fc37aee00fddedbd96
Jesus <jesus.ambrocio@outlook.com>
2023-04-29 08:24:55 -0500

    added delete to deck.

--- README.md
+++ README.md
@@ -3,3 +3,40 @@ Run command 'npm install'
 ?Run command 'npm run setup'
 Run command 'npm start'
 Use end points
+
+PUT: /deck/{idToUpdate}:
+    Only update name and description
+        Object:
+            {
+	            "name": "Updated Pokemon Deck",
+	            "description": "An updated Deck"
+            }
+
+
+
+
+Example Object:
+{
+  "name": "Second Seeding Pokemon Deck",
+  "description": "Second Deck",
+  "cards": [
+    {
+      "hp": 90,
+      "name": "Squirtle",
+      "description": "A water type Pokemon with a turtle-like appearance.",
+      "type": "Water"
+    },
+    {
+      "hp": 80,
+      "name": "Bulbasaur",
+      "description": "A grass/poison type Pokemon with a bulb on its back.",
+      "type": "Grass/Poison"
+    },
+    {
+      "hp": 110,
+      "name": "Charmander",
+      "description": "A fire type Pokemon with a fiery tail.",
+      "type": "Fire"
+    }
+  ]
+}
--- src/routes/deck.ts
+++ src/routes/deck.ts
@@ -25,15 +25,18 @@ router.get("/:deckId", async (req, res) => {
     res.status(500).json({ message: "Something went wrong!" });
   }
 });
+
 router.post("/", async (req, res) => {
   if(!req.body){
     res.send().status(400);
   }
   try{
+    console.log(req.body);
     let savedPokeDeck = await databaseAccess.pokeDeck.create({data:{
       description: req.body.description,
       name: req.body.name,
-      cards: req.body.body.cards
+      cards: {
+        create: req.body.cards}
     }})
     res.status(200).json(savedPokeDeck);
 
@@ -42,5 +45,64 @@ router.post("/", async (req, res) => {
     res.status(500).json({ message: "Something went wrong posting your deck!" });
   }
 })
+
+router.put("/:id", async (req, res) => {
+  if (!req.body) {
+    res.send().status(400);
+  }
+  const { id } = req.params;
+  try {
+    const existingDeck = await databaseAccess.pokeDeck.findUnique({
+      where: { id: Number(id) },
+    });
+    if (!existingDeck) {
+      res.status(404).json({ message: "Deck not found!" });
+      return;
+    }
+    const { name, description } = req.body;
+
+    const updatedDeck = await databaseAccess.pokeDeck.update({
+      where: { id: Number(id) },
+      data: {
+        name: name ? name : existingDeck.name,
+        description: description ? description : existingDeck.description
+    }});
+    res.status(200).json(updatedDeck);
+  } catch (error: any) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Something went wrong updating the deck!" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  if (!req.params.id) {
+    res.send().status(400);
+  }
+  const deckId = Number(req.params.id);
+  const lookUpDeck = await databaseAccess.pokeDeck.findUnique(
+    {
+      where:{
+        id: deckId,
+      }
+    }
+  );
+  if(!lookUpDeck) {
+    res.status(404).json({ message: `Deck with id: ${deckId}, not found.` });
+  };
+
+  try {
+    const deletedDeck = await databaseAccess.pokeDeck.delete({
+      where: {
+        id: deckId
+      }
+    });
+    res.status(200).json(deletedDeck);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong deleting the deck." });
+  }
+});
 
 export default router;
